65793958bde37cc440dadaced8de046b86b01339
ThallapalliLaxmi <laxmi.t@kodebloom.com>
2025-03-10 21:09:55 +0530

    Function.c

---
+++ Function.c
@@ -0,0 +1,14 @@
+Function with arguments and with return values :
+#include<stdio.h>
+int myfun(int a, char b[])
+{
+printf(“%d %s\n”, a, b);
+return 0;
+}
+void main()
+{
+myfun(10 , “KODEBLOOMMM”);
+}
+
+O/P :
+10 KODEBLOOMMM
